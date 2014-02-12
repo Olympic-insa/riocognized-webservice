80ef557d172013a1e6e00c03c43f2a5651a78e8a
alex <alexisd61@gmail.com>
2014-02-12 11:47:08 +0100

    fix in persistence.xml

--- src/main/java/fr/olympicinsa/riocognized/repository/AthleteRepository.java
+++ src/main/java/fr/olympicinsa/riocognized/repository/AthleteRepository.java
@@ -4,7 +4,8 @@ package fr.olympicinsa.riocognized.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.olympicinsa.riocognized.model.Athlete;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface AthleteRepository extends JpaRepository<Athlete, Long> {
 }
