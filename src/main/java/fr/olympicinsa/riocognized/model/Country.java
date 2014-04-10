f563cf06034111e9bee58d110cbf42c715f562ca
alexisduque <alexisd61@gmail.com>
2014-04-10 11:49:54 +0200

    remove unused import

--- src/main/java/fr/olympicinsa/riocognized/model/Country.java
+++ src/main/java/fr/olympicinsa/riocognized/model/Country.java
@@ -1,16 +1,11 @@
 package fr.olympicinsa.riocognized.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import java.util.List;
 import java.io.Serializable;
 import javax.persistence.Column;
-import javax.persistence.FetchType;
 
 @Entity
 @Table(name="COUNTRY")
