c79156f07761d33bfc994bdf333281679b92d8f1
alexisduque <alexisd61@gmail.com>
2014-04-20 08:26:48 +0200

    create Client entity

---
+++ src/main/java/fr/olympicinsa/riocognized/model/Client.java
@@ -0,0 +1,107 @@
+package fr.olympicinsa.riocognized.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.util.Assert;
+
+@Entity
+@Table(name = "CLIENT")
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "LAST_NAME")
+    private String name;
+    @Column(name = "FIRST_NAME")
+    private String surname;
+    @Column(name = "CONTENT")
+    private String content;
+    @ManyToOne
+    private Country country;
+    @JsonIgnore
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @JsonProperty("image_url")
+    public String getURL() {
+        return "http://olympic-insa.fr.nf:8083/image/download/" + image.getId();
+
+    }
+
+}
